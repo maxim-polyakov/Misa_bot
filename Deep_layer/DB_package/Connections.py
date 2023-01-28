face81023392f1c51fbfcbb4bee55ec3b8cba0f0
maxim-polyakov <maxim7012@mail.ru>
2023-01-28 23:28:11 +0300

    Remote_DB_has_been_connected

--- Deep_layer/DB_package/Connections.py
+++ Deep_layer/DB_package/Connections.py
@@ -2,8 +2,11 @@ import psycopg2
 from sqlalchemy import create_engine
 
 class PostgresConnection:
-
-    engine = create_engine(
+    engine_remote = create_engine(
         'postgresql+psycopg2://postgres:postgres@localhost:5432/MisaMemory')
-    conn = psycopg2.connect(
+    conn_remote = psycopg2.connect(
         'dbname=MisaMemory user=postgres password=postgres')
+    engine_remote = create_engine(
+        f'postgresql://postgres:gaTResKPJX25@ep-round-paper-091468.us-east-2.aws.neon.tech/SistersMemory')
+    conn_remote = psycopg2.connect(
+        'postgres://postgres:gaTResKPJX25@ep-round-paper-091468.us-east-2.aws.neon.tech/SistersMemory')
