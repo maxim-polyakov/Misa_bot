f092ac855f04e75de06680d05023548fb102d3de
maxim-polyakov <maxim7012@mail.ru>
2023-01-29 15:15:08 +0300

    Remote_db_tokens_has_been_added

--- Core_layer/Bot_package/TokenAdder.py
+++
@@ -1,9 +0,0 @@
-from Deep_layer.DB_package import DB_Bridge
-
-class TokenAdder:
-    def __init_(cls):
-        pass
-
-    @classmethod
-    def add_token(cls, token):
-        DB_Bridge.DB_Communication.insert_to(str(token), 'tokens')
--- discord_main.py
+++ discord_main.py
@@ -1,12 +1,15 @@
 import Front_layer.discord_bot as db
 from Core_layer.Test_package import PythonTests as PyTest
+from Core_layer.Bot_package import Token
+import nest_asyncio
 # ______________________________________________________________________________
 
 if __name__ == "__main__":
+    tkn = Token.Token()
     test = PyTest.TestRun()
     test.run_all_tests()
-
-    token = ''
-    db.nest_asyncio.apply()
+    df = tkn.get_token('select token from assistant_sets.tokens where botname = \'Misa\' and platformname = \'Discord\'')
+    token = df['token'][0]
+    nest_asyncio.apply()
     db.bot.run(token)
 
