df6defd2036da6876ac1e327dd31ea0d8dfd4cf6
maxim-polyakov <maxim7012@mail.ru>
2023-02-02 21:15:04 +0300

    Update Connections.py

--- Deep_layer/DB_package/Connections.py
+++ Deep_layer/DB_package/Connections.py
@@ -2,10 +2,6 @@ import psycopg2
 from sqlalchemy import create_engine
 
 class PostgresConnection:
-    engine_local = create_engine(
-        'postgresql+psycopg2://postgres:postgres@localhost:5432/MisaMemory')
-    conn_local = psycopg2.connect(
-        'dbname=MisaMemory user=postgres password=postgres')
     engine_remote = create_engine(
         f'postgresql://postgres:gaTResKPJX25@ep-round-paper-091468.us-east-2.aws.neon.tech/SistersMemory')
     conn_remote = psycopg2.connect(
