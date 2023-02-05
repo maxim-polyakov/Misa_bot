ea7f614d22122c0ca7169611acc6ff6ed5ea1687
maxim-polyakov <maxim7012@mail.ru>
2023-02-05 20:47:45 +0300

    Update bototrain.py

--- Front_layer/telegram_bot/bototrain.py
+++ Front_layer/telegram_bot/bototrain.py
@@ -2,30 +2,6 @@ from Front_layer import telegram_bot
 from Core_layer.Bot_package import Bototrainers
 from pathlib import Path
 
-# @telegram_bot.dp.message_handler(commands='emotionstrain')
-# async def get_user_text(message):
-#     if (message.chat.username == 'The_Baxic'):
-#         lt = Bototrainers.LSTMtrain()
-#         bt = Bototrainers.NaiveBayesTrain()
-#         bt.emotionstrain()
-#         lt.emotionstrain(30)
-#         await telegram_bot.boto.send_message(message.chat.id, 'trained', parse_mode='html')
-#     else:
-#         await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
-
-# @telegram_bot.dp.message_handler(commands='NaiveBayestrain')
-# async def get_user_text(message):
-#     if (message.chat.username == 'The_Baxic'):
-#         mt = Bototrainers.NaiveBayesTrain()
-#         mt.hitrain()
-#         mt.thtrain()
-#         mt.businesstrain()
-#         mt.weathertrain()
-#         mt.emotionstrain()
-#         await telegram_bot.boto.send_message(message.chat.id, 'trained', parse_mode='html')
-#     else:
-#         await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
-
 @telegram_bot.dp.message_handler(commands='LSTMemotionstrain')
 async def get_user_text(message):
     if (message.chat.username == 'The_Baxic'):
@@ -95,21 +71,87 @@ async def get_user_text(message):
     else:
         await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
 
-@telegram_bot.dp.message_handler(commands='trashtrain')
+@telegram_bot.dp.message_handler(commands='LSTMweathertrain')
 async def get_user_text(message):
-    resultrainingpath = next(Path().rglob('resultstraining_binary.png'))
-    message.text = message.text.replace('/trashtrain ', ' ')
-    lt = Bototrainers.LSTMtrain()
-    message_text_array = message.text.split()
+    if (message.chat.username == 'The_Baxic'):
+        resultrainingpath = next(Path().rglob('resultstraining_binary.png'))
+        message.text = message.text.replace('/weathertrain ', ' ')
+        lt = Bototrainers.LSTMtrain()
+        message_text_array = message.text.split()
 
-    if (len(message.text) > 0):
-        epochs = message_text_array.pop(0)
-    else:
-        epochs = 200
+        if (len(message.text) > 0):
+            epochs = message_text_array.pop(0)
+        else:
+            epochs = 200
 
-    lt.trashtrain(int(epochs))
-    await telegram_bot.boto.send_message(message.chat.id, 'trashtrain', parse_mode='html')
-    await telegram_bot.boto.send_photo(message.chat.id,
+        lt.weathertrain((int(epochs)))
+        await telegram_bot.boto.send_message(message.chat.id, 'weathertrain', parse_mode='html')
+        await telegram_bot.boto.send_photo(message.chat.id,
                                        photo=open(resultrainingpath, 'rb'))
 
-    await telegram_bot.boto.send_message(message.chat.id, 'trained', parse_mode='html')
+        await telegram_bot.boto.send_message(message.chat.id, 'trained', parse_mode='html')
+    else:
+        await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
+
+@telegram_bot.dp.message_handler(commands='LSTMtrashtrain')
+async def get_user_text(message):
+    if (message.chat.username == 'The_Baxic'):
+        resultrainingpath = next(Path().rglob('resultstraining_binary.png'))
+        message.text = message.text.replace('/trashtrain ', ' ')
+        lt = Bototrainers.LSTMtrain()
+        message_text_array = message.text.split()
+
+        if (len(message.text) > 0):
+            epochs = message_text_array.pop(0)
+        else:
+            epochs = 200
+
+        lt.trashtrain(int(epochs))
+        await telegram_bot.boto.send_message(message.chat.id, 'trashtrain', parse_mode='html')
+        await telegram_bot.boto.send_photo(message.chat.id,
+                                       photo=open(resultrainingpath, 'rb'))
+
+        await telegram_bot.boto.send_message(message.chat.id, 'trained', parse_mode='html')
+    else:
+        await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
+
+@telegram_bot.dp.message_handler(commands='LSTMthtrain')
+async def get_user_text(message):
+    if (message.chat.username == 'The_Baxic'):
+        resultrainingpath = next(Path().rglob('resultstraining_binary.png'))
+        message.text = message.text.replace('/trashtrain ', ' ')
+        lt = Bototrainers.LSTMtrain()
+        message_text_array = message.text.split()
+
+        if (len(message.text) > 0):
+            epochs = message_text_array.pop(0)
+        else:
+            epochs = 200
+
+        lt.thtrain(int(epochs))
+        await telegram_bot.boto.send_message(message.chat.id, 'thtrain', parse_mode='html')
+        await telegram_bot.boto.send_photo(message.chat.id,
+                                           photo=open(resultrainingpath, 'rb'))
+    else:
+        await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
+
+@telegram_bot.dp.message_handler(commands='businesstrain')
+async def get_user_text(message):
+    if (message.chat.username == 'The_Baxic'):
+        resultrainingpath = next(Path().rglob('resultstraining_binary.png'))
+        message.text = message.text.replace('/LSTMbusinesstrain ', ' ')
+        lt = Bototrainers.LSTMtrain()
+        message_text_array = message.text.split()
+
+        if(len(message.text) > 0):
+            epochs = message_text_array.pop(0)
+        else:
+            epochs = 200
+
+        lt.businesstrain(int(epochs))
+
+        await telegram_bot.boto.send_message(message.chat.id, 'businesstrain', parse_mode='html')
+        await telegram_bot.boto.send_photo(message.chat.id,
+                                           photo=open(resultrainingpath, 'rb'))
+    else:
+        await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
