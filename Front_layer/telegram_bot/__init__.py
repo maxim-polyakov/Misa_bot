835cb9711ccf33ad635179a33dd6c0989d938743
maxim-polyakov <maxim7012@mail.ru>
2023-02-06 19:38:30 +0300

    Update __init__.py

--- Front_layer/telegram_bot/__init__.py
+++ Front_layer/telegram_bot/__init__.py
@@ -1,8 +1,11 @@
 from Front_layer.telegram_bot import *
 from aiogram import Bot, Dispatcher
 from Core_layer.Bot_package import Token
-
+from telebot import apihelper
 # ______________________________________________________________________________
+
+apihelper.proxy = {'https': 'socks5://telegram.vpn.net:55555'}
+
 tkn = Token.Token()
 df = tkn.get_token('select token from assistant_sets.tokens where botname = \'Misa\' and platformname = \'Telegram\'')
 API_TOKEN = df['token'][0]
