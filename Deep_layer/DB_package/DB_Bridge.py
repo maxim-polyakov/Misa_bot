abf01459e2e44d6ab54f19e17a139bbd1f3b79f0
maxim-polyakov <maxim7012@mail.ru>
2023-02-12 15:26:40 +0300

    Update DB_Bridge.py

--- Deep_layer/DB_package/DB_Bridge.py
+++ Deep_layer/DB_package/DB_Bridge.py
@@ -28,14 +28,16 @@ class DB_Communication(IDB_Communication):
     @dispatch(object, object, object, object, object, object, object)
     def insert_to(cls, text, tablename, string, agenda, classification, classtype):
         try:
-            pr = TextPreprocessers.CommonPreprocessing()
-            data = {'text': pr.preprocess_text(
-                text), agenda: string, classification: classtype}
-            df = pd.DataFrame()
-            new_row = pd.Series(data)
-            df = df.append(new_row, ignore_index=True)
-            df.to_sql(tablename, con=Connections.PostgresConnection.engine_remote,
-                      schema='recognized_sets', index=False, if_exists='append')
+            with Connections.PostgresConnection.conn_remote.cursor():
+                pr = TextPreprocessers.CommonPreprocessing()
+                data = {'text': pr.preprocess_text(
+                    text), agenda: string, classification: classtype}
+                df = pd.DataFrame()
+                new_row = pd.Series(data)
+                df = df.append(new_row, ignore_index=True)
+                df.to_sql(tablename, con=Connections.PostgresConnection.engine_remote,
+                        schema='recognized_sets', index=False, if_exists='append')
+            Connections.PostgresConnection.conn_remote.close()
         except:
             print("exception is in DB_Communication.insert_to")
 
@@ -43,13 +45,16 @@ class DB_Communication(IDB_Communication):
     @dispatch(object, int, object, object, object)
     def insert_to(cls, idx, txt, insert, datatable):
         try:
-            insert = insert.split(', ')
-            data = {'id': idx, 'text': txt, 'agenda': insert[0], 'emotion': insert[1]}
-            df = pd.DataFrame()
-            new_row = pd.Series(data)
-            df = df.append(new_row, ignore_index=True)
-            df.to_sql(datatable, con=Connections.PostgresConnection.engine_remote, schema='validation_sets',
-                      index=False, if_exists='append')
+            with Connections.PostgresConnection.conn_remote.cursor():
+                insert = insert.split(', ')
+                data = {'id': idx, 'text': txt, 'agenda': insert[0], 'emotion': insert[1]}
+                df = pd.DataFrame()
+                new_row = pd.Series(data)
+                df = df.append(new_row, ignore_index=True)
+                df.to_sql(datatable, con=Connections.PostgresConnection.engine_remote, schema='validation_sets',
+                        index=False, if_exists='append')
+            Connections.PostgresConnection.conn_remote.close()
+
         except:
             print("exception is in DB_Communication.insert_to")
 
@@ -57,8 +62,10 @@ class DB_Communication(IDB_Communication):
     @dispatch(object, object, object, object)
     def insert_to(cls, df, datatable, schema):
         try:
-            df.to_sql(datatable, con=Connections.PostgresConnection.engine_remote, schema=schema,
-                      index=False, if_exists='append')
+            with Connections.PostgresConnection.conn_remote.cursor():
+                df.to_sql(datatable, con=Connections.PostgresConnection.engine_remote, schema=schema,
+                          index=False, if_exists='append')
+            Connections.PostgresConnection.conn_remote.close()
         except:
             print("exception is in DB_Communication.insert_to")
 
@@ -66,8 +73,10 @@ class DB_Communication(IDB_Communication):
     @dispatch(object, object, object)
     def insert_to(cls, df, datatable):
        try:
-           df.to_sql(datatable, con=Connections.PostgresConnection.engine_remote, schema='assistant_sets',
+           with Connections.PostgresConnection.conn_remote.cursor():
+                df.to_sql(datatable, con=Connections.PostgresConnection.engine_remote, schema='assistant_sets',
                      index=False, if_exists='append')
+           Connections.PostgresConnection.conn_remote.close()
        except:
            print("exception is in DB_Communication.insert_to")
 
@@ -75,15 +84,17 @@ class DB_Communication(IDB_Communication):
     @dispatch(object, str, str)
     def insert_to(cls, string, datatable):
        try:
-           dfvalid = pd.read_sql('select count(tokens) from assistant_sets.' + datatable,
+           with Connections.PostgresConnection.conn_remote.cursor():
+                dfvalid = pd.read_sql('select count(tokens) from assistant_sets.' + datatable,
                                  Connections.PostgresConnection.conn_remote)
-           count = dfvalid['count'][0]
-           data = {'id': count + 1, 'token': string}
-           df = pd.DataFrame()
-           new_row = pd.Series(data)
-           df = df.append(new_row, ignore_index=True)
-           df.to_sql(datatable, con=Connections.PostgresConnection.engine_remote, schema='assistant_sets',
-                     index=False, if_exists='append')
+                count = dfvalid['count'][0]
+                data = {'id': count + 1, 'token': string}
+                df = pd.DataFrame()
+                new_row = pd.Series(data)
+                df = df.append(new_row, ignore_index=True)
+                df.to_sql(datatable, con=Connections.PostgresConnection.engine_remote, schema='assistant_sets',
+                          index=False, if_exists='append')
+           Connections.PostgresConnection.conn_remote.close()
        except:
            print("exception is in DB_Communication.insert_to")
 
@@ -91,27 +102,31 @@ class DB_Communication(IDB_Communication):
     @dispatch(object, object)
     def insert_to(cls, lowertext):
         try:
-            def insert(table, count, text, schema):
-                for txt in text:
-                    data = {'id': count + 1,'text': txt}
-                    df = pd.DataFrame()
-                    new_row = pd.Series(data)
-                    df = df.append(new_row, ignore_index=True)
-                    df.to_sql(table, con=Connections.PostgresConnection.engine_remote, schema=schema,
+            with Connections.PostgresConnection.conn_remote.cursor():
+                def insert(table, count, text, schema):
+                    for txt in text:
+                        data = {'id': count + 1,'text': txt}
+                        df = pd.DataFrame()
+                        new_row = pd.Series(data)
+                        df = df.append(new_row, ignore_index=True)
+                        df.to_sql(table, con=Connections.PostgresConnection.engine_remote, schema=schema,
                             index=False, if_exists='append')
-            lowertext = lowertext.replace('миса ', '').replace('misa ', '')
-            text = []
-            dfmesstor = pd.read_sql('select count(text) from messtorage.storage',
+                lowertext = lowertext.replace('миса ', '').replace('misa ', '')
+                text = []
+                dfmesstor = pd.read_sql('select count(text) from messtorage.storage',
                                 Connections.PostgresConnection.conn_remote)
-            counts = dfmesstor['count'][0]
-            text.append(lowertext)
-            insert('storage', counts, text, 'messtorage')
+                counts = dfmesstor['count'][0]
+                text.append(lowertext)
+                insert('storage', counts, text, 'messtorage')
+            Connections.PostgresConnection.conn_remote.close()
         except:
             print("exception is in DB_Communication.get_data")
 
     @classmethod
     def get_data(cls, select):
-        df = pd.read_sql(select, Connections.PostgresConnection.conn_remote)
+        with Connections.PostgresConnection.conn_remote.cursor():
+            df = pd.read_sql(select, Connections.PostgresConnection.conn_remote.cursor())
+        Connections.PostgresConnection.conn_remote.close()
         return df
 
 
@@ -128,11 +143,13 @@ class DB_Communication(IDB_Communication):
     @classmethod
     def checkcommands(cls, input_string):
         try:
-            df = pd.read_sql('SELECT text FROM assistant_sets.commands', Connections.PostgresConnection.conn_remote)
-            Cdict = df['text'].to_dict()
-            for cdictvalue in Cdict.values():
-                if(cdictvalue in input_string):
-                    return True
-            return False
+            with Connections.PostgresConnection.conn_remote.cursor():
+                df = pd.read_sql('SELECT text FROM assistant_sets.commands', Connections.PostgresConnection.conn_remote)
+                Cdict = df['text'].to_dict()
+                for cdictvalue in Cdict.values():
+                    if(cdictvalue in input_string):
+                        return True
+                return False
+            Connections.PostgresConnection.conn_remote.close()
         except:
             print("exception is in DB_Communication.checkcommands")
