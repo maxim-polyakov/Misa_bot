e4bc8092f2a48c42f1373ef6389cddbe2ebdb79b
maxim-polyakov <maxim7012@mail.ru>
2023-02-14 20:27:57 +0300

    RF_XGB_models_has_come_back

--- Core_layer/Test_package/PythonTests/TestCase_NLP_package.py
+++ Core_layer/Test_package/PythonTests/TestCase_NLP_package.py
@@ -1,5 +1,7 @@
 from Core_layer.Test_package.PythonTests import ITestCase
-from Deep_layer.NLP_package.Models.Binary import NaiveBayes
+from Deep_layer.NLP_package.Models.Multy import NaiveBayes
+#from Deep_layer.NLP_package.Models.Binary import NaiveBayes
+#from Deep_layer.NLP_package.Models.Binary import NaiveBayes
 from Core_layer.Bot_package import Selects
 import unittest
 
@@ -8,6 +10,15 @@ class TestCase_NLP_package(ITestCase.ITestCase):
     sel = Selects.Select()
 
     def test_nbmodel_train(self):
+
+        filemodel = './models/binary/NaiveBayes/himodel.pickle'
+        filetokenizer = './tokenizers/binary/NaiveBayes/hivec.pickle'
+        datasetfile = self.sel.SELECT_HI
+        trainer = NaiveBayes.NaiveBayes(filemodel, filetokenizer, datasetfile)
+        trainer.train('hi')
+
+    def test_nbmodel_train(self):
+
 
         filemodel = './models/binary/NaiveBayes/himodel.pickle'
         filetokenizer = './tokenizers/binary/NaiveBayes/hivec.pickle'
---
+++ Deep_layer/NLP_package/Models/Multy/RandomForest.py
@@ -0,0 +1,50 @@
+import pickle as p
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from Deep_layer.DB_package.DB_Bridge import DB_Communication
+from Deep_layer.NLP_package.Models import IModel
+from Deep_layer.NLP_package.Tokenizers import Tokenizer as t
+
+class RandomForest(IModel.IModel):
+
+    def __init__(cls, filemodelname, tokenizerfilename, dataselect,
+                 recognizeddataselect):
+        RandomForest.__filemodelname = filemodelname
+        RandomForest.__tokenizerfilename = tokenizerfilename
+        RandomForest.__dataselect = dataselect
+        RandomForest.__recognizeddataselect = recognizeddataselect
+    @classmethod
+    def __createmodel(cls):
+        rfc = RandomForestClassifier(criterion='entropy', n_estimators=700)
+        return rfc
+
+    @classmethod
+    def train(cls, target):
+        try:
+            train = DB_Communication.DB_Communication.get_data(cls.__dataselect)
+            train.text = train.text.astype(str)
+
+            df = pd.concat([train])
+
+            train = df[~df[target].isna()]
+            train[target] = train[target].astype(int)
+            train = train.drop_duplicates()
+
+            print(train)
+
+            X_train, X_val, Y_train, Y_val = train_test_split(train, train[target], test_size=0.3, random_state=32)
+            print('Shape of train', X_train.shape)
+            print('Shape of Validation ', X_val.shape)
+            tokenizer = t.Tokenizer(train_texts=X_train['text'])
+            tokenizer.train_tokenize()
+            tokenized_X_train = tokenizer.vectorize_input(X_train['text'])
+            rfc = cls.__createmodel()
+            rfc.fit(tokenized_X_train, Y_train)
+            with open(cls.__tokenizerfilename, 'wb') as handle:
+                p.dump(tokenizer, handle, protocol=p.HIGHEST_PROTOCOL)
+
+            with open(cls.__filemodelname, 'wb') as handle:
+                p.dump(rfc, handle, protocol=p.HIGHEST_PROTOCOL)
+        except:
+            print('The exception is in RandomForest.train')
