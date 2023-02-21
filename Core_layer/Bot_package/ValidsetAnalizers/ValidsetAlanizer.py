090464b29a17a4e86b786752007bc7971d471dcb
maxim-polyakov <maxim7012@mail.ru>
2023-02-21 22:52:43 +0300

    testmonitorchanges

--- Core_layer/Bot_package/ValidsetAnalizers/ValidsetAlanizer.py
+++ Core_layer/Bot_package/ValidsetAnalizers/ValidsetAlanizer.py
@@ -9,33 +9,58 @@ class ValidsetAlanizer(IAnalyzer.IAnalyzer):
     @classmethod
     @dispatch(object)
     def analyze(cls):
-        outdict = {'LSTMACC': []}
+        outdict = {'LSTMACC': [], 'RFACC': [], 'NBACC': [], 'XGBACC': []}
 
         humandf = DB_Communication.DB_Communication.get_data(
             'SELECT id, text, agenda from validation_sets.markedvalidsethuman ORDER BY id ASC')
+
         lstmdf = DB_Communication.DB_Communication.get_data(
             'SELECT id, text, agenda from validation_sets.markedvalidsetlstm ORDER BY id ASC')
 
-        rdf = DB_Communication.pd.DataFrame(
-            {'id': humandf['id'], 'text': humandf['text'], 'agendalstm': lstmdf['agenda'], 'agendahuman': humandf['agenda']})
+        rfdf = DB_Communication.DB_Communication.get_data(
+            'SELECT id, text, agenda from validation_sets.markedvalidsetrandomforest ORDER BY id ASC')
+
+        nbdf = DB_Communication.DB_Communication.get_data(
+            'SELECT id, text, agenda from validation_sets.markedvalidsetnaivebayes ORDER BY id ASC')
+
+        xgbdf = DB_Communication.DB_Communication.get_data(
+            'SELECT id, text, agenda from validation_sets.markedvalidsetxgboost ORDER BY id ASC')
+
+        rdf = DB_Communication.pd.DataFrame({
+             'id': humandf['id'],
+             'text': humandf['text'],
+             'agendalstm': lstmdf['agenda'],
+             'agendarf': rfdf['agenda'],
+             'agendanb': nbdf['agenda'],
+             'agendaxgb': xgbdf['agenda'],
+             'agendahuman': humandf['agenda']
+             })
 
         DB_Communication.DB_Communication.delete_data('DELETE FROM assistant_sets.analyzetable')
 
         DB_Communication.DB_Communication.insert_to(rdf, 'analyzetable')
 
         select = str("select distinct * from assistant_sets.analyzetable " +
-                     "where agendahuman  not like 'Команда' " +
-                     "and agendalstm is not null ")
+                     "where agendalstm " +
+                     "not like 'Команда' is not null " +
+                     "and agendanb  is not null " +
+                     "and agendalstm  is not null " +
+                     "and agendarf  is not null " +
+                     "and agendaxgb is not null")
 
         df = DB_Communication.DB_Communication.get_data(select)
 
         outdict['LSTMACC'].append(str(accuracy_score(df['agendalstm'], df['agendahuman'])))
+        outdict['RFACC'].append(str(accuracy_score(df['agendarf'], df['agendahuman'])))
+        outdict['NBACC'].append(str(accuracy_score(df['agendanb'], df['agendahuman'])))
+        outdict['XGBACC'].append(str(accuracy_score(df['agendaxgb'], df['agendahuman'])))
+
         return outdict
 
     @classmethod
     @dispatch(object, object)
     def analize(cls, analyzetable):
-        outdict = {'LSTMACC': []}
+        outdict = {'LSTMACC': [], 'RFACC': [], 'NBACC': [], 'XGBACC': []}
 
         select = str("select distinct * from assistant_sets.analyzetable " +
                      "where agendahuman  not like 'Команда' " +
@@ -44,4 +69,8 @@ class ValidsetAlanizer(IAnalyzer.IAnalyzer):
         df = DB_Communication.DB_Communication.get_data(select)
 
         outdict['LSTMACC'].append(str(accuracy_score(df['agendalstm'], df['agendahuman'])))
+        outdict['RFACC'].append(str(accuracy_score(df['agendarf'], df['agendahuman'])))
+        outdict['NBACC'].append(str(accuracy_score(df['agendanb'], df['agendahuman'])))
+        outdict['XGBACC'].append(str(accuracy_score(df['agendaxgb'], df['agendahuman'])))
+
         return outdict
---
+++ Core_layer/Test_package/TestMonitors/TestMonitorXGB.py
@@ -0,0 +1,19 @@
+from Deep_layer.DB_package.DB_Bridge import DB_Communication
+from Core_layer.Test_package.TestMonitors import TestMonitor
+
+class TestMonitorXGB(TestMonitor.TestMonitor):
+
+    @classmethod
+    def monitor(cls):
+
+        DB_Communication.DB_Communication.delete_data(
+            'DELETE FROM validation_sets.markedvalidsetxgboost')
+
+        df = DB_Communication.DB_Communication.get_data(
+            'SELECT id, text from validation_sets.markedvalidsethuman ORDER BY id ASC')
+
+        datatable = 'markedvalidsetxgboost'
+
+        modelpath = '/binary/XgBoost/'
+
+        super().monitor(df, datatable, modelpath)
