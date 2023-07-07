2919d996f4532674d788ae3ddb5d8e4b0d3509d6
maxim-polyakov <maxim7012@mail.ru>
2023-07-07 19:16:52 +0300

    123

--- Front_layer/telegram_bot/picturemonitor.py
+++ Front_layer/telegram_bot/picturemonitor.py
@@ -2,6 +2,6 @@ from Front_layer import telegram_bot
 
 @telegram_bot.dp.message_handler(content_types=['photo'])
 async def photo(message):
-   idphoto = message.photo[0].file_id
-
-   await telegram_bot.boto.send_photo(message.chat.id, idphoto)
+    file = telegram_bot.boto.getFile(message.photo.file_id)
+    print ("file_id: " + str(message.photo.file_id))
+    file.download('image.jpg')
