21cefc06c4cacb4c22f085af6a2c9db1e1875e85
maxim-polyakov <maxim7012@mail.ru>
2023-09-19 17:20:45 +0300

    fix

--- Front_layer/telegram_bot/bototrain/xgbtrain.py
+++ Front_layer/telegram_bot/bototrain/xgbtrain.py
@@ -4,97 +4,112 @@ from pathlib import Path
 
 @telegram_bot.dp.message_handler(commands='XGBtrain')
 async def get_user_text(message):
-    if (message.chat.username == 'The_Baxic'):
+    try:
+        if (message.chat.username == 'The_Baxic'):
 
-        xgb = XGBtrain.XGBtrain()
-        await xgb.hitrain()
-        await telegram_bot.boto.send_message(message.chat.id, 'hitrain', parse_mode='html')
+            xgb = XGBtrain.XGBtrain()
+            await xgb.hitrain()
+            await telegram_bot.boto.send_message(message.chat.id, 'hitrain', parse_mode='html')
 
-        await xgb.thtrain()
-        await telegram_bot.boto.send_message(message.chat.id, 'thtrain', parse_mode='html')
+            await xgb.thtrain()
+            await telegram_bot.boto.send_message(message.chat.id, 'thtrain', parse_mode='html')
 
-        await xgb.businesstrain()
-        await telegram_bot.boto.send_message(message.chat.id, 'businesstrain', parse_mode='html')
+            await xgb.businesstrain()
+            await telegram_bot.boto.send_message(message.chat.id, 'businesstrain', parse_mode='html')
 
-        await xgb.weathertrain()
-        await telegram_bot.boto.send_message(message.chat.id, 'weathertrain', parse_mode='html')
+            await xgb.weathertrain()
+            await telegram_bot.boto.send_message(message.chat.id, 'weathertrain', parse_mode='html')
 
-        await xgb.trashtrain()
-        await telegram_bot.boto.send_message(message.chat.id, 'trashtrain', parse_mode='html')
+            await xgb.trashtrain()
+            await telegram_bot.boto.send_message(message.chat.id, 'trashtrain', parse_mode='html')
 
-        await telegram_bot.boto.send_message(message.chat.id, 'trained', parse_mode='html')
-    else:
-        await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
-
+            await telegram_bot.boto.send_message(message.chat.id, 'trained', parse_mode='html')
+        else:
+            await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
+    except:
+        pass
 
 @telegram_bot.dp.message_handler(commands='RFweathertrain')
 async def get_user_text(message):
-    if (message.chat.username == 'The_Baxic'):
-        xgb = XGBtrain.XGBtrain()
+    try:
+        if (message.chat.username == 'The_Baxic'):
+            xgb = XGBtrain.XGBtrain()
 
-        await xgb.weathertrain()
-        await telegram_bot.boto.send_message(message.chat.id, 'weathertrain', parse_mode='html')
-    else:
-        await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
-
+            await xgb.weathertrain()
+            await telegram_bot.boto.send_message(message.chat.id, 'weathertrain', parse_mode='html')
+        else:
+            await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
+    except:
+        pass
 
 @telegram_bot.dp.message_handler(commands='RFtrashtrain')
 async def get_user_text(message):
-    if (message.chat.username == 'The_Baxic'):
-        xgb = XGBtrain.XGBtrain()
+    try:
+        if (message.chat.username == 'The_Baxic'):
+            xgb = XGBtrain.XGBtrain()
 
-        await xgb.trashtrain()
-        await telegram_bot.boto.send_message(message.chat.id, 'trained', parse_mode='html')
-    else:
-        await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
-
+            await xgb.trashtrain()
+            await telegram_bot.boto.send_message(message.chat.id, 'trained', parse_mode='html')
+        else:
+            await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
+    except:
+        pass
 
 @telegram_bot.dp.message_handler(commands='RFhitrain')
 async def get_user_text(message):
-    if (message.chat.username == 'The_Baxic'):
-        xgb = XGBtrain.XGBtrain()
+    try:
+        if (message.chat.username == 'The_Baxic'):
+            xgb = XGBtrain.XGBtrain()
 
-        await xgb.hitrain()
-        await telegram_bot.boto.send_message(message.chat.id, 'hitrain', parse_mode='html')
-    else:
-        await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
-
+            await xgb.hitrain()
+            await telegram_bot.boto.send_message(message.chat.id, 'hitrain', parse_mode='html')
+        else:
+            await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
+    except:
+        pass
 
 @telegram_bot.dp.message_handler(commands='RFthtrain')
 async def get_user_text(message):
 #
 #
-    if (message.chat.username == 'The_Baxic'):
-        xgb = XGBtrain.XGBtrain()
+    try:
+        if (message.chat.username == 'The_Baxic'):
+            xgb = XGBtrain.XGBtrain()
 
-        await xgb.thtrain()
-        await telegram_bot.boto.send_message(message.chat.id, 'thtrain', parse_mode='html')
-    else:
-        await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
-
+            await xgb.thtrain()
+            await telegram_bot.boto.send_message(message.chat.id, 'thtrain', parse_mode='html')
+        else:
+            await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
+    except:
+        pass
 
 @telegram_bot.dp.message_handler(commands='RFbusinesstrain')
 async def get_user_text(message):
 #
 #
-    if (message.chat.username == 'The_Baxic'):
-        xgb = XGBtrain.XGBtrain()
+    try:
+        if (message.chat.username == 'The_Baxic'):
+            xgb = XGBtrain.XGBtrain()
 
-        await xgb.businesstrain()
-        await telegram_bot.boto.send_message(message.chat.id, 'businesstrain', parse_mode='html')
+            await xgb.businesstrain()
+            await telegram_bot.boto.send_message(message.chat.id, 'businesstrain', parse_mode='html')
+            pass
+        else:
+            await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
+    except:
         pass
-    else:
-        await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
-
 
 @telegram_bot.dp.message_handler(commands='RFemotionstrain')
 async def get_user_text(message):
 #
 #
-    if (message.chat.username == 'The_Baxic'):
-        xgb = XGBtrain.XGBtrain()
+    try:
+        if (message.chat.username == 'The_Baxic'):
+            xgb = XGBtrain.XGBtrain()
 
-        await xgb.emotionstrain()
-        await telegram_bot.boto.send_message(message.chat.id, 'trained', parse_mode='html')
-    else:
-        await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
+            await xgb.emotionstrain()
+            await telegram_bot.boto.send_message(message.chat.id, 'trained', parse_mode='html')
+        else:
+            await telegram_bot.boto.send_message(message.chat.id, '😊', parse_mode='html')
+    except:
+        pass
