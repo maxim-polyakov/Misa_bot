cf2499d48574185b79888289acf8ff795fc0c0fd
maxim-polyakov <maxim7012@gmail.com>
2024-07-17 21:02:09 +0300

    fix

--- Deep_layer/DB_package/Classes/DB_Communication.py
+++ Deep_layer/DB_package/Classes/DB_Communication.py
@@ -1,28 +1,59 @@
 import pandas as pd
+from Deep_layer.NLP_package.TextPreprocessers import CommonPreprocessing
 from multipledispatch import dispatch
-from Deep_layer.DB_package.Classes import Connections
-from Deep_layer.DB_package.Inerfaces import IDB_Communication
-from Deep_layer.NLP_package.Classes.TextPreprocessers import CommonPreprocessing
+from Deep_layer.DB_package.DB_Bridge import IDB_Communication, Connections
 import psycopg2
 
 
 class DB_Communication(IDB_Communication.IDB_Communication):
-    """
 
-    Summary
+    @classmethod
+    @dispatch(object, object, object, object, object, object, object)
+    def insert_to(cls, text, tablename, string, agenda, classification, classtype):
+#
+#
+        try:
+            postgr_conn = Connections.PostgresConnection()
+            pr = CommonPreprocessing.CommonPreprocessing()
+            data = {'text': pr.preprocess_text(
+                text), agenda: string, classification: classtype}
+            df = pd.DataFrame()
+            new_row = pd.Series(data)
+            df = df.append(new_row, ignore_index=True)
+            df.to_sql(tablename, con=postgr_conn.engine_remote,
+                schema='recognized_sets', index=False, if_exists='append')
+        except:
+            print("exception is in DB_Communication.insert_to")
 
-    """
+    @classmethod
+    @dispatch(object, int, object, object, object)
+    def insert_to(cls, idx, txt, insert, datatable):
+#
+#
+        try:
+            postgr_conn = Connections.PostgresConnection()
+            insert = insert.split(', ')
+            data = {'id': idx, 'text': txt, 'agenda': insert[0], 'emotion': insert[1]}
+            df = pd.DataFrame()
+            new_row = pd.Series(data)
+            df = df.append(new_row, ignore_index=True)
+            df.to_sql(datatable, con=postgr_conn.engine_remote, schema='validation_sets',
+                index=False, if_exists='append')
 
+        except psycopg2.OperationalError:
+            print("exception is in DB_Communication.insert_to")
 
     @classmethod
     @dispatch(object, object, object, object)
     def insert_to(cls, df, datatable, schema):
 #
 #
-        postgr_conn = Connections.PostgresConnection()
-        df.to_sql(datatable, con=postgr_conn.engine_remote, schema=schema,
-            index=False, if_exists='append')
-
+        try:
+            postgr_conn = Connections.PostgresConnection()
+            df.to_sql(datatable, con=postgr_conn.engine_remote, schema=schema,
+                index=False, if_exists='append')
+        except:
+            print("exception is in DB_Communication.insert_to")
 
     @classmethod
     @dispatch(object, object, object)
@@ -43,7 +74,13 @@ class DB_Communication(IDB_Communication.IDB_Communication):
 #
        try:
             postgr_conn = Connections.PostgresConnection()
+            dfvalid = pd.read_sql('select count(tokens) from assistant_sets.' + datatable,
+                            postgr_conn.conn_remote)
+            count = dfvalid['count'][0]
+            data = {'id': count + 1, 'token': string}
             df = pd.DataFrame()
+            new_row = pd.Series(data)
+            df = df.append(new_row, ignore_index=True)
             df.to_sql(datatable, con=postgr_conn.engine_remote, schema='assistant_sets',
                         index=False, if_exists='append')
        except psycopg2.OperationalError:
@@ -64,7 +101,6 @@ class DB_Communication(IDB_Communication.IDB_Communication):
                     df = df.append(new_row, ignore_index=True)
                     df.to_sql(table, con=postgr_conn.engine_remote, schema=schema,
                         index=False, if_exists='append')
-
             lowertext = lowertext.replace('миса ', '').replace('misa ', '')
             text = []
             dfmesstor = pd.read_sql('select count(text) from messtorage.storage',
@@ -111,6 +147,24 @@ class DB_Communication(IDB_Communication.IDB_Communication):
             Cdict = df['text'].to_dict()
             for cdictvalue in Cdict.values():
                 if(cdictvalue in input_string):
+                    return True
+            return False
+        except psycopg2.OperationalError:
+            print("exception is in DB_Communication.checkcommands")
+
+    @classmethod
+    def check(cls, input_string, table):
+        #
+        #
+        try:
+            pr = CommonPreprocessing.CommonPreprocessing()
+            postgr_conn = Connections.PostgresConnection()
+            df = pd.read_sql('SELECT text FROM train_sets.' + table, postgr_conn.conn_remote)
+            Cdict = df['text'].to_dict()
+            inpt = []
+            inpt.append(pr.preprocess_text(input_string[0]))
+            for cdictvalue in Cdict.values():
+                if (cdictvalue in inpt):
                     return True
             return False
         except psycopg2.OperationalError:
