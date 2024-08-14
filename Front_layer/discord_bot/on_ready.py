0b90ef4ba8730b9faa31f2c82be016ab3637b13a
maxim-polyakov <maxim7012@mail.ru>
2024-08-14 15:17:31 +0300

    Update on_ready.py

--- Front_layer/discord_bot/on_ready.py
+++ Front_layer/discord_bot/on_ready.py
@@ -4,8 +4,7 @@ from asyncio import sleep #не забываем
 
 @discord_bot.bot.event
 async def on_ready():
-    while True:
-        await discord_bot.bot.change_presence(
-            status=discord_bot.discord.Status.online,
-            activity=discord_bot.discord.CustomActivity("Смотрит за сервером"))
-        await sleep(15)
+    await discord_bot.bot.change_presence(
+        status=discord_bot.discord.Status.online,
+        activity=discord_bot.discord.CustomActivity("Смотрит за сервером"))
+    await sleep(15)
