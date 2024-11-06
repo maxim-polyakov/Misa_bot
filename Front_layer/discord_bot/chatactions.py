cc20aab158258b07ccba2d92916ca8712dcd1382
maxim-polyakov <maxim7012@mail.ru>
2024-11-06 17:24:15 +0300

    Update chatactions.py

--- Front_layer/discord_bot/chatactions.py
+++ Front_layer/discord_bot/chatactions.py
@@ -1,6 +1,7 @@
 from Front_layer import discord_bot
 from Core_layer.Bot_package.Classes.Monitors.AudioMonitors import SongsMonitor
 import os
+
 def remove_all_files(dir):
     for f in os.listdir(dir):
         os.remove(os.path.join(dir, f))
