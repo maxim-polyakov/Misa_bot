d6ca12e98963328647f699298d714b4eded7b884
maxim-polyakov <maxim7012@mail.ru>
2024-11-19 18:47:03 +0300

    Code refactor changes

---
+++ Core_layer/Command_package/Classes/CommandActions/AActions/AActionEight.py
@@ -0,0 +1,126 @@
+import logging
+from Core_layer.Answer_package.Classes import RandomAnswer
+from Deep_layer.NLP_package.Classes.TextPreprocessers import CommonPreprocessing
+from Core_layer.Command_package.Interfaces import IAction
+from Deep_layer.API_package.Classes.Finders import WikiFinder
+from Deep_layer.API_package.Classes.Finders import GoogleFinder
+from Deep_layer.API_package.Classes.Calculators import SympyCalculator
+from Deep_layer.API_package.Classes.Translators import GoogleTranslator
+from Deep_layer.API_package.Classes.WeatherPredictors import WeatherPredictor
+from Deep_layer.NLP_package.Classes.TextPreprocessers import CommonPreprocessing, Preprocessing
+
+
+class AActionEight(IAction.IAction):
+    """
+    It is class for comand's actions started on symbol a
+    """
+    boto = None
+    message = None
+    message_text = None
+
+    __pred = Preprocessing.Preprocessing()
+    __pr = CommonPreprocessing.CommonPreprocessing()
+
+    def __init__(self, message, message_text):
+        AActionEight.message = message
+        AActionEight.message_text = message_text
+
+    @classmethod
+    def first(cls):
+#
+#
+        logging.basicConfig(level=logging.INFO, filename="misa.log", filemode="w")
+        try:
+            pass
+        except Exception as e:
+            logging.exception(str('The exception in aactionf.first ' + str(e)))
+
+    @classmethod
+    def second(cls):
+#
+#
+        logging.basicConfig(level=logging.INFO, filename="misa.log", filemode="w")
+        try:
+            pass
+        except Exception as e:
+            logging.exception(str('The exception in aactionf.second ' + str(e)))
+
+    @classmethod
+    def third(cls):
+#
+#
+        logging.basicConfig(level=logging.INFO, filename="misa.log", filemode="w")
+        try:
+            pass
+        except Exception as e:
+            logging.exception(str('The exception in aactionf.third ' + str(e)))
+
+    @classmethod
+    def fourth(cls):
+#
+#
+        logging.basicConfig(level=logging.INFO, filename="misa.log", filemode="w")
+        try:
+            pass
+        except Exception as e:
+            logging.exception(str('The exception in aactionf.fourth ' + str(e)))
+
+    @classmethod
+    def fifth(cls):
+#
+#
+        logging.basicConfig(level=logging.INFO, filename="misa.log", filemode="w")
+        try:
+            pass
+        except Exception as e:
+            logging.exception(str('The exception in aactionf.fifth ' + str(e)))
+
+    @classmethod
+    def sixth(cls):
+#
+#
+        logging.basicConfig(level=logging.INFO, filename="misa.log", filemode="w")
+        try:
+            pass
+        except Exception as e:
+            logging.exception(str('The exception in aactionf.sixth ' + str(e)))
+
+    @classmethod
+    def seventh(cls):
+#
+#
+        logging.basicConfig(level=logging.INFO, filename="misa.log", filemode="w")
+        try:
+            pass
+        except Exception as e:
+            logging.exception(str('The exception in aactionf.seventh ' + str(e)))
+
+    @classmethod
+    def eighth(cls):
+#
+#
+        logging.basicConfig(level=logging.INFO, filename="misa.log", filemode="w")
+        try:
+            pass
+        except Exception as e:
+            logging.exception(str('The exception in aactionf.eighth ' + str(e)))
+
+    @classmethod
+    def nineth(cls):
+#
+#
+        logging.basicConfig(level=logging.INFO, filename="misa.log", filemode="w")
+        try:
+            pass
+        except Exception as e:
+            logging.exception(str('The exception in aactionf.nineth ' + str(e)))
+
+    @classmethod
+    def tenth(cls):
+#
+#
+        logging.basicConfig(level=logging.INFO, filename="misa.log", filemode="w")
+        try:
+            pass
+        except Exception as e:
+            logging.exception(str('The exception in aactionf.tenth ' + str(e)))
