2f6fdee22e7ed2956957a27351e4fc2945fd0a98
Baxic <maxim7012@gmail.com>
2024-12-07 01:25:08 +0300

    1

--- Core_layer/Test_package/Classes/PythonTests/TestCase_NLP_package.py
+++ Core_layer/Test_package/Classes/PythonTests/TestCase_NLP_package.py
@@ -1,6 +1,7 @@
 import unittest
 import logging
 from Deep_layer.NLP_package.Classes.TextPreprocessers import CommonPreprocessing
+from Deep_layer.NLP_package.Classes.GPT import Gpt
 from Core_layer.Test_package.Interfases import ITestCase
 
 
@@ -11,12 +12,13 @@ class TestCase_API_package(ITestCase.ITestCase):
 
     """
     pr = CommonPreprocessing.CommonPreprocessing()
+    gpt = Gpt.Gpt()
 
     def test_commonpreprocessing(self):
         res = self.pr.preprocess_text('поздоровайся')
         self.assertEqual(res, 'поздороваться')
 
     def test_gpt(self):
-        pass
+        res = self.gpt.generate('это тест')
 if __name__ == '__main__':
     unittest.main()
