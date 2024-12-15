492dc494d3c43438b7dbc8af373bc4def737b485
Baxic <maxim7012@gmail.com>
2024-12-15 22:19:57 +0300

    Update Dalle.py

--- Deep_layer/IOD_package/Classes/Dalle.py
+++ Deep_layer/IOD_package/Classes/Dalle.py
@@ -9,6 +9,8 @@ class Dalle(IGpt.IGpt):
     It is a gpt text generator
     """
     __dbc = DB_Communication.DB_Communication()
+    __pr = CommonPreprocessing.CommonPreprocessing()
+
     @classmethod
     def generate(cls, text):
 #
@@ -21,6 +23,20 @@ class Dalle(IGpt.IGpt):
             OPENAI_API_KEY = fdf['token'][0]
             OPENAI_API_PROJECT = sdf['token'][0]
             OPENAI_API_ORG = tdf['token'][0]
+            client = OpenAI(
+                api_key=OPENAI_API_KEY,
+                organization=OPENAI_API_ORG,
+                project=OPENAI_API_PROJECT,
+            )
+            input_text = cls.__pr.preprocess_text(text)
+            response = client.images.generate(
+                model="dall-e-3",
+                prompt=input_text,
+                size="1024x1024",
+                quality="standard",
+                n=1,
+            )
+            return response.data[0].url
 
         except Exception as e:
             logging.exception(str('The exception is in gpt.generate ' + str(e)))
