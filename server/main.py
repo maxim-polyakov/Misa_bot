55971fd95309f3b7af4f93fbc197b2aea01df63a
maxim-polyakov <maxim7012@gmail.com>
2025-09-04 22:30:00 +0300

    server

---
+++ server/main.py
@@ -0,0 +1,13 @@
+import nltk
+
+
+import Front_layer.django_server as dj
+from Core_layer.Test_package.Classes.PythonTests import TestRun as PyTest
+
+# ______________________________________________________________________________
+
+if __name__ == "__main__":
+#
+#
+    pass
+
