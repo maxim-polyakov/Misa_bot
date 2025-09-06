d3d8bf0db059efe3794773da44a96e4c44a4665c
maxim-polyakov <maxim7012@gmail.com>
2025-09-06 11:58:13 +0300

    Update GoogleFinder.py

--- Core_layer/Bot_package/Classes/Finder/GoogleFinder.py
+++ Core_layer/Bot_package/Classes/Finder/GoogleFinder.py
@@ -10,8 +10,10 @@ class GoogleFinder(IFinder.IFinder):
 
     """
     message_text = None
+
     def __init__(self, message_text):
         GoogleFinder.message_text = message_text
+
     @classmethod
     def find(cls):
         # finding by google funder
@@ -22,18 +24,20 @@ class GoogleFinder(IFinder.IFinder):
             gpif = gfinder.GoogleFinder()
             # perform a search using googlefinder
             finded_list = gpif.find(cls.message_text)
-            # prepare the output string
+            # prepare the output string with proper capitalization
             outstr = 'Ссылки по запросу:\n'
             # check if any results were found
-            if (finded_list != None):
+            if finded_list:
                 for outmes in finded_list:
                     outstr += outmes + ' \n '
+            else:
+                return 'Не нашла'
+
             # log that the search operation was completed successfully
             logging.info('The googlefinder.find process has completed successfully')
-            # if no results were found, return a message indicating that
-            if outstr == '':
-                return 'Не нашла'
             return outstr
+
         except Exception as e:
             # log any exceptions that occur during the search process
             logging.exception('The exception occurred in googlefinder.find: ' + str(e))
+            return 'Произошла ошибка при поиске'
