f231931923bcc7f37849801705b1a586d3ce99cd
maxim-polyakov <maxim7012@gmail.com>
2025-09-06 21:31:31 +0300

    Update AppRouter.js

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,16 +1,32 @@
 import {Context} from "../index.js";
 import { useContext } from "react";
-import { Route, Routes } from "react-router-dom";
-import { authRoutes, publickRoutes } from "../Routes.js";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { authRoutes, publicRoutes } from "../Routes.js";
 import { observer } from "mobx-react-lite";
 import Chat from "../pages/Chat.js";
 
 const AppRouter = observer(() => {
     return (
         <Routes>
-            <Route path="*" element={<Chat></Chat>}></Route>
-        </Routes>
+            {/* Редирект с корневой страницы на чат */}
+            <Route path="/" element={<Navigate to="/chat" replace />} />
 
+            {/* Только этот маршрут ведет в чат */}
+            <Route path="/chat" element={<Chat />} />
+
+            {/* Все остальные маршруты (если они есть в ваших routes.js) */}
+            {publicRoutes.map(({path, Component}) =>
+                <Route key={path} path={path} element={<Component />} />
+            )}
+
+            {/* Защищенные маршруты (если нужно) */}
+            {authRoutes.map(({path, Component}) =>
+                <Route key={path} path={path} element={<Component />} />
+            )}
+
+            {/* Дефолтный маршрут - можно оставить или изменить */}
+            <Route path="*" element={<div>Страница не найдена</div>} />
+        </Routes>
     );
 });
 
