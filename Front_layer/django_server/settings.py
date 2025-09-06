77e7b2713c356a220b6b48f964081f4a46a06468
maxim-polyakov <maxim7012@gmail.com>
2025-09-06 13:25:09 +0300

    Update settings.py

--- Front_layer/django_server/settings.py
+++ Front_layer/django_server/settings.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -6,6 +9,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'channels'
 ]
+
+# Предполагаем, что BASE_DIR это корень проекта (где manage.py)
+BASE_DIR = Path(__file__).resolve().parent.parent.parent  # Или подстройте под вашу структуру
+
+# URL для доступа к статическим файлам
+STATIC_URL = '/'
+
+# Директории, где Django ищет статические файлы, кроме app/static/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'images'),  # Укажите путь к вашей папке images
+]
+
 ROOT_URLCONF = 'Front_layer.django_server.urls'
 ASGI_APPLICATION = 'Front_layer.django_server.asgi.application'
 ALLOWED_HOSTS = ['*']
