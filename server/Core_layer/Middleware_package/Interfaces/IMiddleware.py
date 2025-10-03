c0407a31ad0127bd1a793c1ea5f0ce21f97af9be
maxim-polyakov <maxim7012@gmail.com>
2025-10-03 14:06:36 +0300

    Update IMiddleware.py

--- server/Core_layer/Middleware_package/Interfaces/IMiddleware.py
+++ server/Core_layer/Middleware_package/Interfaces/IMiddleware.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class IMiddleware(ABC):
     """
-    It is entity of controller
+    It is entity of middleware
     """
     @abstractmethod
     def verify_token_and_get_user(cls):
