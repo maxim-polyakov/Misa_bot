96084f98a3889137050ce1354ce3fd97c3b0c2e6
maxim-polyakov <maxim7012@gmail.com>
2026-03-13 21:44:27 +0300

    г

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { applyTheme } from './utils/theme.js';
 import UserChat from "./store/userChat";
+import { LocaleProvider } from './contexts/LocaleContext.js';
 import App from './App.js';
 
 applyTheme();
@@ -15,6 +16,8 @@ root.render(
     <Context.Provider value={{
         user: new UserChat()
     }}>
-        <App />
+        <LocaleProvider>
+            <App />
+        </LocaleProvider>
     </Context.Provider>
 );
